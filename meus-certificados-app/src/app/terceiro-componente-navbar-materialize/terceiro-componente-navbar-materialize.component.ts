f9b3c857bc212a083f728061ebcfe4588b95ef06
MichelBatistadeMoraes-PC <micmorba@hotmail.com>
2023-06-10 17:35:17 -0300

    Atividade 08 - comit point save

---
+++ meus-certificados-app/src/app/terceiro-componente-navbar-materialize/terceiro-componente-navbar-materialize.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as M from "materialize-css";
+
+@Component({
+  selector: 'app-terceiro-componente-navbar-materialize',
+  templateUrl: './terceiro-componente-navbar-materialize.component.html',
+  styleUrls: ['./terceiro-componente-navbar-materialize.component.scss']
+})
+export class TerceiroComponenteNavbarMaterializeComponent {
+ 
+ 
+}
